bf48bcb8ca88a7368212a8d89477e5daf24e926c
Hamza417 <HamzaRizwan243@gmail.com>
2024-03-20 19:13:16 +0530

    Initial Commit

--- app/src/main/java/app/simple/inurelauncher/ui/screens/HomeScreen.kt
+++ app/src/main/java/app/simple/inurelauncher/ui/screens/HomeScreen.kt
@@ -6,21 +6,23 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.material3.VerticalDivider
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import app.simple.inurelauncher.R
@@ -50,6 +52,7 @@ fun HomeScreen(context: Context, navController: NavController? = null) {
                 DockItem(context = context, packageName = "app.simple.peri", modifier = Modifier.weight(1f))
                 DockItem(context = context, packageName = "app.simple.positional", modifier = Modifier.weight(1f))
                 DockItem(context = context, packageName = "app.simple.positional", modifier = Modifier.weight(1f))
+                VerticalDivider(Modifier.height(64.dp))
                 AppDrawerButton(modifier = Modifier
                     .weight(1F)
                     .clickable {
@@ -91,11 +94,19 @@ fun DockItem(context: Context, packageName: String, modifier: Modifier = Modifie
 
 @Composable
 fun AppDrawerButton(modifier: Modifier = Modifier) {
-    Image(painter = painterResource(id = R.drawable.ic_shape_line),
-          contentDescription = "",
-          modifier = modifier
-              .size(64.dp)
-    )
+    Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceEvenly,
+            modifier = modifier
+                .clickable {
+
+                }
+                .padding(8.dp),
+    ) {
+        Image(imageVector = ImageVector.vectorResource(id = R.drawable.ic_drawer),
+             contentDescription = "Apps",
+             modifier = Modifier.size(64.dp))
+    }
 }
 
 @Preview
