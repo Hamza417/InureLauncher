86c7e27e4239d1e822780dacf341faaf9169c046
Hamza417 <HamzaRizwan243@gmail.com>
2024-03-20 17:01:25 +0530

    Initial Commit

--- app/src/main/java/app/simple/inurelauncher/ui/screens/HomeScreen.kt
+++ app/src/main/java/app/simple/inurelauncher/ui/screens/HomeScreen.kt
@@ -1,22 +1,23 @@
 package app.simple.inurelauncher.ui.screens
 
 import android.content.Context
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material3.Card
-import androidx.compose.material3.Icon
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
@@ -38,13 +39,18 @@ fun HomeScreen(context: Context, navController: NavController? = null) {
                     .wrapContentHeight(align = androidx.compose.ui.Alignment.Bottom),
         ) {
             // Dock
-            Row {
-                DockItem(context = context, packageName = "app.simple.inure")
-                DockItem(context = context, packageName = "app.simple.peri")
-                DockItem(context = context, packageName = "app.simple.positional")
-                AppDrawerButton(modifier = Modifier.clickable {
-                    navController?.navigate(Routes.APP_DRAWER)
-                })
+            Row(
+                    verticalAlignment = androidx.compose.ui.Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.SpaceEvenly,
+            ) {
+                DockItem(context = context, packageName = "app.simple.inure", modifier = Modifier.weight(1f))
+                DockItem(context = context, packageName = "app.simple.peri", modifier = Modifier.weight(1f))
+                DockItem(context = context, packageName = "app.simple.positional", modifier = Modifier.weight(1f))
+                AppDrawerButton(modifier = Modifier
+                    .weight(1F)
+                    .clickable {
+                        navController?.navigate(Routes.APP_DRAWER)
+                    })
             }
         }
     }
@@ -55,7 +61,7 @@ fun HomeScreen(context: Context, navController: NavController? = null) {
 fun DockItem(context: Context, packageName: String, modifier: Modifier = Modifier) {
     // Dock item
     Column(
-            modifier = Modifier
+            modifier = modifier
                 .padding(8.dp)
                 .clickable {
                     val applicationInfo = context.packageManager.getApplicationInfo(packageName, 0)
@@ -66,19 +72,24 @@ fun DockItem(context: Context, packageName: String, modifier: Modifier = Modifie
                         }
                 },
     ) {
-        GlideImage(model = AppIcon(context, packageName),
-                   contentDescription = "",
-                   modifier = Modifier
-                       .height(64.dp)
-                       .width(64.dp))
+        GlideImage(
+                model = AppIcon(context, packageName),
+                contentDescription = "",
+                modifier = Modifier
+                    .height(64.dp)
+                    .width(64.dp),
+                alignment = Alignment.Center
+        )
     }
 }
 
 @Composable
 fun AppDrawerButton(modifier: Modifier = Modifier) {
-    Icon(imageVector = ImageVector.vectorResource(R.drawable.ic_shape_line),
-         contentDescription = "",
-         modifier.height(64.dp).width(64.dp).fillMaxHeight())
+    Image(painter = painterResource(id = R.drawable.ic_shape_line),
+          contentDescription = "",
+          modifier = modifier
+              .size(64.dp)
+    )
 }
 
 @Preview
